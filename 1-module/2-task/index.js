444addc9f644b4bd57ea02e601878c7304d515c9
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-05-26 04:44:40 -0400

    add task 2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,11 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name && !name.includes(" ") && name.length >= 4) {
+    return true;
+  }
+
+  return false;
 }
 
 function sayHello() {
