6cefe3a07e3198fd00a48842f0da2338fcf92efc
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-05-26 05:07:00 -0400

    add task 4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
-  // ваш код...
+  let spamText1 = "1xbet";
+  let spamText2 = "xxx";
+
+  let text = str.toLowerCase();
+  if (text.includes(spamText1) || text.includes(spamText2)) {
+    return true;
+  }
+
+  return false;
 }
