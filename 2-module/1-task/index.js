aea683c6e0fcdf5ef2bc2cfa3e13a5d49f8ac3b5
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-05-28 06:34:23 -0400

    add task 1 salary sum

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let salariesSum = 0;
+  for (let key in salaries) {
+    if (key != "currency" && key != "month" && key != "isPayed") {
+      if (salaries[key] != NaN && salaries[key] != Infinity && salaries[key] != -Infinity) {
+        salariesSum += salaries[key];
+      }
+    }
+  }
+
+  return salariesSum;
 }
