5e69dfe75a740411760480dd73f8adb591f21189
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-05-28 07:02:03 -0400

    fix isNaN check

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,8 +1,8 @@
 function sumSalary(salaries) {
   let salariesSum = 0;
   for (let key in salaries) {
-    if (key != "currency" && key != "month" && key != "isPayed") {
-      if (salaries[key] != NaN && salaries[key] != Infinity && salaries[key] != -Infinity) {
+    if (key !== "currency" && key !== "month" && key !== "isPayed") {
+      if (isNaN(salaries[key]) && salaries[key] !== Infinity && salaries[key] !== -Infinity) {
         salariesSum += salaries[key];
       }
     }
@@ -10,3 +10,18 @@ function sumSalary(salaries) {
 
   return salariesSum;
 }
+
+
+    let salaries = {
+      John: 1000,
+      Ann: 1600,
+      Pete: 1300,
+      Bob: NaN,
+      Peter: Infinity,
+      Ivan: -Infinity,
+      month: 'December',
+      currency: 'USD',
+      isPayed: false
+    };
+
+console.log(sumSalary(salaries));
