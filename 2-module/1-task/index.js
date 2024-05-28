4c00cab19673f49a5621ef55e81c7988e19ae54e
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-05-28 07:04:35 -0400

    add "not" operator

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -2,7 +2,7 @@ function sumSalary(salaries) {
   let salariesSum = 0;
   for (let key in salaries) {
     if (key !== "currency" && key !== "month" && key !== "isPayed") {
-      if (isNaN(salaries[key]) && salaries[key] !== Infinity && salaries[key] !== -Infinity) {
+      if (!isNaN(salaries[key]) && salaries[key] !== Infinity && salaries[key] !== -Infinity) {
         salariesSum += salaries[key];
       }
     }
@@ -10,18 +10,3 @@ function sumSalary(salaries) {
 
   return salariesSum;
 }
-
-
-    let salaries = {
-      John: 1000,
-      Ann: 1600,
-      Pete: 1300,
-      Bob: NaN,
-      Peter: Infinity,
-      Ivan: -Infinity,
-      month: 'December',
-      currency: 'USD',
-      isPayed: false
-    };
-
-console.log(sumSalary(salaries));
