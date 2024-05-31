c8278b77aae7a07c117f96786875759e1dbd68d3
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-05-31 18:29:50 -0400

    add filter range

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,24 @@
+/*
 function filterRange(arr, a, b) {
-  // ваш код...
+  const filteredArray = [];
+  for (const number of arr) {
+    if (number >= a && number <=b) {
+      filteredArray.push(number);
+    }
+  }
+
+  return filteredArray;
 }
+*/
+
+function filterRange(arr, a, b) {
+  return arr.filter(item => item >= a && item <= b);
+}
+
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+console.log( filtered ); // [3,1] (совпадающие значения)
+console.log( arr ); // [5,3,8,1] (без изменений)
