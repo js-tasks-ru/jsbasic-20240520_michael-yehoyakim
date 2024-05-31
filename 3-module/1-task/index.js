db52f31aabcac26dbcf8ce2a7307b6d22d08c12f
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-05-31 17:42:38 -0400

    add namify users

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,31 @@
+/*
 function namify(users) {
-  // ваш код...
+  const names = [];
+  for (const user of users) {
+    names.push(user.name);
+  }
+
+  return names;
 }
+*/
+
+/*
+function namify(users) {
+  const names = [];
+  for ({name} of users) {
+    names.push(name);
+  }
+  return names;
+}*/
+
+function namify(users) {
+  return users.map(user => user.name);
+}
+
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = namify(users); // ['Вася', 'Петя', 'Маша']
