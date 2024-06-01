cd83030ee4f743f6aeb0f32b1c2b76a664402d8e
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-05-31 21:23:42 -0400

    update sort method params

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -2,7 +2,7 @@ function getMinMax(str) {
   const arr = str.split(" ")
     .map(strValue => Number(strValue))
     .filter(value => Boolean(value))
-    .sort();
+    .sort((a, b) => a - b);
 
   return {min: arr[0], max: arr[arr.length -1]};
 }
