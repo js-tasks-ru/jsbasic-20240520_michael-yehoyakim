a3be773a5c3fda6acec6a713204a060538860278
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-05-31 21:23:25 -0400

    remove unused function

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,10 +3,6 @@ function camelize(str) {
   return arr.shift() + arr.map(item => item[0].toUpperCase() + item.slice(1)).join("");
 }
 
-function capitalize(str) {
-  return str[0].toUpperCase() + str.slice(1);
-}
-
 camelize('background-color') == 'backgroundColor';
 camelize('list-style-image') == 'listStyleImage';
 camelize('-webkit-transition') == 'WebkitTransition';
