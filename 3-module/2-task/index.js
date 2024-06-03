f885ad2fd5f7ffbe9828788902acc72ab86fca46
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-06-03 11:31:09 -0400

    clean up

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,24 +1,3 @@
-/*
-function filterRange(arr, a, b) {
-  const filteredArray = [];
-  for (const number of arr) {
-    if (number >= a && number <=b) {
-      filteredArray.push(number);
-    }
-  }
-
-  return filteredArray;
-}
-*/
-
 function filterRange(arr, a, b) {
   return arr.filter(item => item >= a && item <= b);
 }
-
-
-let arr = [5, 3, 8, 1];
-
-let filtered = filterRange(arr, 1, 4);
-
-console.log( filtered ); // [3,1] (совпадающие значения)
-console.log( arr ); // [5,3,8,1] (без изменений)
--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,7 +2,3 @@ function camelize(str) {
   arr = str.split("-");
   return arr.shift() + arr.map(item => item[0].toUpperCase() + item.slice(1)).join("");
 }
-
-camelize('background-color') == 'backgroundColor';
-camelize('list-style-image') == 'listStyleImage';
-camelize('-webkit-transition') == 'WebkitTransition';
