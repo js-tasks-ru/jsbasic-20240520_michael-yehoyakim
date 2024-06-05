19af9d647c3d7b3b1e424866ad91994d63098081
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-06-05 12:39:57 -0400

    add makeFriendsList function

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,37 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  // need to create a template
+  // then add the create an element from a template and return it
+
+  const ulElement = document.createElement("ul");
+
+  let listTemplate = ``;
+
+  for (let {firstName, lastName} of friends) {
+    listTemplate += `<li>${firstName} ${lastName}</li>`;
+  }
+
+  ulElement.innerHTML = listTemplate;
+
+  return ulElement
+
+
 }
+
+
+
+let friends = [
+    {
+        firstName: 'Artsiom',
+        lastName: 'Mezin'
+    },
+    {
+        firstName: 'Ilia',
+        lastName: 'Kantor'
+    },
+    {
+        firstName: 'Christopher',
+        lastName: 'Michael'
+    }
+];
+
+makeFriendsList(friends);
