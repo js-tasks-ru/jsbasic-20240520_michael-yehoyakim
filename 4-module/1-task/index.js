d6d1add49836d99e8fe9d8bd93fbe992921616d1
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-06-05 12:45:42 -0400

    Revert "add makeFriendsList function"
    This reverts commit 19af9d647c3d7b3b1e424866ad91994d63098081.

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,37 +1,3 @@
 function makeFriendsList(friends) {
-  // need to create a template
-  // then add the create an element from a template and return it
-
-  const ulElement = document.createElement("ul");
-
-  let listTemplate = ``;
-
-  for (let {firstName, lastName} of friends) {
-    listTemplate += `<li>${firstName} ${lastName}</li>`;
-  }
-
-  ulElement.innerHTML = listTemplate;
-
-  return ulElement
-
-
+  // ваш код...
 }
-
-
-
-let friends = [
-    {
-        firstName: 'Artsiom',
-        lastName: 'Mezin'
-    },
-    {
-        firstName: 'Ilia',
-        lastName: 'Kantor'
-    },
-    {
-        firstName: 'Christopher',
-        lastName: 'Michael'
-    }
-];
-
-makeFriendsList(friends);
