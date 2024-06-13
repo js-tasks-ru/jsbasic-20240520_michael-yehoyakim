3d818dd7954f81dc48df794e45ba5fbc74a4897f
Michael Yehoyakim <myehoyakim@plushcare.com>
2024-06-13 18:13:35 -0400

    add task 1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,66 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
+function createElement(html) {
+  const temp = document.createElement('div');
+  temp.innerHTML = html;
+  return temp.firstElementChild;
+}
+
 export default class UserTable {
+  elem = null;
+  #rows = [];
+
   constructor(rows) {
+    this.#rows = rows;
+    this.headers = Object.keys(this.#rows[0]);
+    this.data = this.#rows.map(item => Object.values(item));
+    this.elem = this.render();
+  }
+
+  template() {
+    let tdData = [];
+
+    for (let row of this.data) {
+      const trData = row.map(v => `<td>${v}</td>`).join('') + '<td><button class="remove-button">X</button></td>';
+      tdData.push(trData);
+    }
+
+    const table = `
+    <table>
+        <thead>
+            <tr>
+                ${this.headers.map(value => `<th>${value}</th>`).join('')}
+            </tr>
+        </thead>
+        <tbody>
+              ${tdData.map(value => `<tr>${value}</tr>`).join('')}
+        </tbody>
+    </table>
+    `;
+
+    return table;
+  }
+
+  onButtonClick = (event) => {
+    if (event.target.className != 'remove-button') {
+      return;
+    }
+
+    const button = event.target;
+    const row = button.closest('tr');
+
+    if (!row) {
+      return;
+    }
+
+    row.remove();
+  }
+
+  render () {
+    this.elem = createElement(this.template());
+
+    this.elem.addEventListener('click', this.onButtonClick);
+
+    return this.elem;
+
   }
 }
+
